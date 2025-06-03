25cba21f047ec99f639b9c99d3485830a8c891ab
Lucas Sirineu <sirineu33@gmail.com>
2025-06-02 23:04:31 -0300

    Atualizando Program.cs com as últimas alterações

--- Program.cs
+++ Program.cs
@@ -42,17 +42,5 @@ namespace Estudo
             gato.Apresentar();
         }
     }
-    public class Animal
-    {
-        public string Nome { get; set; }
 
-        public Animal(string nome)
-        {
-            Nome = nome;
-        }
-        public virtual void Apresentar()
-        {
-            Console.WriteLine("Som genérico de animal");
-        }
-    }
 }
